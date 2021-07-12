49318a6796386393cd2975754782742ef94f0d88
altyn <altynbek.shagdarbek@gmail.com>
2021-07-12 09:27:57 +0600

    qwe

--- src/main/java/kz/bcc/tuto/bpm/BpmApplication.java
+++ src/main/java/kz/bcc/tuto/bpm/BpmApplication.java
@@ -3,9 +3,11 @@ package kz.bcc.tuto.bpm;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableProcessApplication
+@EnableEurekaClient
 public class BpmApplication {
 
     public static void main(String[] args) {
